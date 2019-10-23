67780f3f19b302107df114c02233c9ee4b52d2d3
anhhuynh <anhhuynh681@gmail.com>
2019-10-24 01:30:01 +0700

    login

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -31,7 +31,7 @@ const  auth= {
     //auth.password=password
     axios.post('https://neunhuladinhmenh.herokuapp.com/api/user/login', JSON.stringify(auth),
     {
-        headers: {         "Content-Type":"application/json",
+        headers: {   "Accept":"application/json",      "Content-Type":"application/json",
     
           }
     })
