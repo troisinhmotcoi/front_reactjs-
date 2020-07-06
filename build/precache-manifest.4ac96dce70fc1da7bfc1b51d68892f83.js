32690a35fa8bb00ac4dc33041f6dbb68318df4c9
troisinhmotcoi <anhhuynh681@gmail.com>
2020-07-07 00:57:05 +0700

    build

---
+++ build/precache-manifest.4ac96dce70fc1da7bfc1b51d68892f83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546dbf155c071db07efc7c8361f2f6da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3136e357e2d613de8edf",
+    "url": "/static/css/main.56753675.chunk.css"
+  },
+  {
+    "revision": "ab1bc3cbf2d2bc6240e3",
+    "url": "/static/js/2.66771c54.chunk.js"
+  },
+  {
+    "revision": "3136e357e2d613de8edf",
+    "url": "/static/js/main.280d78a0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
